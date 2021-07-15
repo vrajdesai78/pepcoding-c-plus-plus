8c1e07a9cbce2fd137843b36206832bed5332685
vrajdesai78 <vrajdesai78@gmail.com>
2021-07-15 10:54:20 +0530

    Fibonacci Program added

---
+++ Basics of programming/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int first=0;
+    int second=1;
+    cout<<first<<endl;
+    cout<<second<<endl;
+    for(int i=0;i<n-2;i++)
+    {
+       int temp = first+second;
+       cout<<temp<<endl;
+       first = second;
+       second = temp;
+    }
+}
